53b8944f9f867dbd993c69713b4dc24677f91ac7
sychev2003di <sychev2003di@gmail.com>
2024-09-19 17:54:53 +0300

    lab1: add read from/write to file

--- lab 1/main.kt
+++ lab 1/main.kt
@@ -1,11 +1,59 @@
 package encryption
 
-import kotlin.math.abs
+import java.io.File
 
-fun main() {
-    val shifr = CezareWithKeyWord("АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ", 3, "ШИФРОВКА")
-    val encoded_line = shifr.encode("НЕПТУН")
-    val decoded_line = shifr.decode(encoded_line)
+val FILE_PATH = arrayOf(
+    arrayOf(
+        """files/first key.txt""",
+        """files/first line.txt""",
+        """files/first encode.txt""",
+        """files/first decode.txt""",
+    ),
+    arrayOf( 
+        """files/second key.txt""",
+        """files/second line.txt""",
+        """files/second encode.txt""",
+        """files/second decode.txt""",
+    ),
+)
+
+fun first_algorithm() {
+    val key = File(FILE_PATH[0][0]).readLines().first().toInt()
+    println(key)
+    val shifr = Scytale(key)
+    val line = File(FILE_PATH[0][1]).readLines().first()
+    println(line)
+    val encoded_line = shifr.encode(line)
+    val decoded_line = shifr.decode(encoded_line) 
+
+    File(FILE_PATH[0][2]).writeText(encoded_line)
+    File(FILE_PATH[0][3]).writeText(decoded_line)
+
     println(encoded_line)
     println(decoded_line)
+}
+
+fun second_algorithm() {
+    val alphabet: String
+    val offset: Int
+    val keyword: String
+    val lines = File(FILE_PATH[1][0]).readLines()
+    println(lines)
+    alphabet = lines[0]
+    offset = lines[1].toInt()
+    keyword = lines[2]
+
+    val line = File(FILE_PATH[1][1]).readLines().first()
+    println(line)
+
+    val shifr = CezareWithKeyWord(alphabet, offset, keyword)
+    val encoded_line = shifr.encode(line)
+    val decoded_line = shifr.decode(encoded_line)
+
+    File(FILE_PATH[1][2]).writeText(encoded_line)
+    File(FILE_PATH[1][3]).writeText(decoded_line)
+}
+
+fun main() {
+    first_algorithm()
 }
